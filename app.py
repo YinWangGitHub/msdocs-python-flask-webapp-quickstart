09368dd49c5ca4764c1da3558a1868accb7855ac
Yin Wang <yinwang@smu.edu>
2023-08-31 19:46:12 -0500

    added some changes

--- app.py
+++ app.py
@@ -1,32 +1,80 @@
-import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import requests, os, uuid, json
+#from dotenv import load_dotenv
+#load_dotenv()
+from flask import Flask, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
 
 
-@app.route('/')
+KEY= "0615a14f92e444aab800e922b8d07ea2"
+ENDPOINT= "https://api.cognitive.microsofttranslator.com/"
+LOCATION= "eastus"
+
+
+@app.route('/', methods=['GET'])
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/', methods=['POST'])
+def index_post():
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    previous_language = 'zh-CN'  # 假设为示例值
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Read the values from the form
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    ### Load the values from .env
+    ##key = os.environ['KEY']
+    ##endpoint = os.environ['ENDPOINT']
+    ##location = os.environ['LOCATION']
+
+    key = KEY
+    endpoint = ENDPOINT
+    location = LOCATION
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate?api-version=3.0'
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information, which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    # Call render template, passing the translated text,
+    # original text, and target language to the template
+    return render_template(
+        'index.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language,
+        previous_language=previous_language
+   
+ )
+
 
 
 if __name__ == '__main__':
    app.run()
+    
+
+
+
